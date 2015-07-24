26ad5640b1c2274dc8fd1e7f50f9f1c94fe84030
Lucas Medina <lucass.medina@gmail.com>
2015-07-24 02:28:22 -0300

    Finishing workflow settings

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,7 +1,7 @@
 module.exports = function(grunt) {
 
 	grunt.initConfig({
-			
+		
 		connect : {
 			server : {
 				options : {
@@ -19,36 +19,81 @@ module.exports = function(grunt) {
 					style : 'compact'
 				},
 				files : {
-					'build/css/main.css' : 'build/src/style.scss'
+					'build/css/main.css' : 'src/sass/style.scss'
 				}
+			}
+		},
+
+		autoprefixer : {
+			options : {
+				browsers: ['last 2 versions', 'ie 8', 'ie 9']
+			},
+			files : {
+				'build/css/main.css' : 'build/css/main.css'
+			}
+		},
+
+		copy : {
+			thirdparty : {
+				expand : true,
+				cwd : 'src/thirdparty',
+				src : '**',
+				dest  : 'build'
 			}
 		},
 
 		imagemin : {
 			dist : {
+				expand : true,
+				cwd : 'src/images/',
+				src : '**/*.*',
+				dest : 'build/images/'
+			}
+		},
+
+		uglify : {
+			dist : {
+				files : {
+					'build/js/main.min.js' : ['src/js/main.js']
+				}
+			},
+			finish : {
 				options : {
-					optimizationLevel : 3,
+					compress : {
+						drop_console : true
+					}
 				},
-				files : [{
-					expand: true,
-					cwd : 'src/',
-					src : ['images/*.*'],
-					dest : 'build/'
-				}]
+				files : {
+					'build/js/main.min.js' : ['src/js/main.js']
+				}
 			}
 		},
 
 		watch : {
-			css : {
-				files : 'src/sass/**',
-				tasks : ['sass'],
+			images : {
+				files : ['src/images/**'],
+				tasks : ['imagemin']
+			},
+			thirdparty : {
+				files : ['src/thirdparty/**'],
+				tasks : ['copy:thirdparty'],
 				options : {
 					livereload : true
 				}
 			},
-			images : {
-				files : 'src/images/**',
-				tasks : ['imagemin']
+			css : {
+				files : ['src/sass/**'],
+				tasks : ['sass', 'autoprefixer'],
+				options : {
+					livereload : true
+				}
+			},
+			js : {
+				files : ['src/js/**'],
+				tasks : ['uglify:dist'],
+				options : {
+					livereload : true
+				}
 			}
 		}
 
@@ -56,12 +101,13 @@ module.exports = function(grunt) {
 
 	grunt.loadNpmTasks('grunt-contrib-connect');
 	grunt.loadNpmTasks('grunt-contrib-sass');
+	grunt.loadNpmTasks('grunt-autoprefixer');
 	grunt.loadNpmTasks('grunt-contrib-imagemin');
+	grunt.loadNpmTasks('grunt-contrib-copy');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-watch');
 
-	grunt.registerTask('default', ['sass', 'imagemin']);
+	grunt.registerTask('default', ['sass', 'autoprefixer', 'imagemin', 'copy', 'uglify:dist']);
 	grunt.registerTask('update', ['connect', 'watch']);
-	grunt.registerTask('seer', ['sass', 'imagemin', 'connect', 'watch']);
-
-	
+	grunt.registerTask('finish', ['sass', 'autoprefixer', 'imagemin', 'copy', 'uglify:finish']);
 }
